30e8b69d4945c185c589259df9d58e388f62a526
nikhil-shrivastava <nikhil.shrivastav33@gmail.com>
2024-10-03 10:03:54 +0530

    minor updates

--- scripts/mainscript.py
+++ scripts/mainscript.py
@@ -1,16 +1,5 @@
 from dataingestion import DataIngestion
 import pandas as pd
-from db_script import Database
-# from data_cleaning import DataCleaning
-
-
-def DataCleaning(df):
-    df=df.fillna(df.mean(), inplace=True)
-    # df=df.drop_duplicates(inplace=True)
-    # df = df.rename(columns=lambda x: x.strip())
-    print('printing df: ', df)
-    df['date'] = pd.to_datetime(df['date'])
-    return df
 
 di = DataIngestion()
 di.csv_df('data/csv_input.csv')
